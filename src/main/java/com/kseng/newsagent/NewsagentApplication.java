18f8bad4c2a23abba92a29148989c9a2fce435de
James Clarke <jjjclarke@yahoo.com>
2026-01-22 13:14:24 +0000

    Initial commit
    Signed-off-by: James Clarke <jjjclarke@yahoo.com>

---
+++ src/main/java/com/kseng/newsagent/NewsagentApplication.java
@@ -0,0 +1,13 @@
+package com.kseng.newsagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsagentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsagentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kseng/newsagent/NewsagentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kseng.newsagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsagentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
